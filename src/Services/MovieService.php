f7265138fbb4ffec5a161c49229035be8ce1a3df
anthomas63 <anthomas63@gmail.com>
2021-11-09 11:43:14 +0100

    :zap: add more movies for testing

--- src/Services/MovieService.php
+++ src/Services/MovieService.php
@@ -40,7 +40,49 @@ class MovieService
 En 2001, quatre millions d'années plus tard, un vaisseau spatial évolue en orbite lunaire au rythme langoureux du \"Beau Danube Bleu\". A son bord, le Dr. Heywood Floyd enquête secrètement sur la découverte d'un monolithe noir qui émet d'étranges signaux vers Jupiter.",
             "category" => "Sciences-fiction",
             "image" => "images/movies/odyssee_espace.jpg",
-        ]
+        ],
+        [
+            "title" => "Blade Runner",
+            "description" => "2019, Los Angeles. La Terre est surpeuplée et l’humanité est partie coloniser l’espace. Les nouveaux colons sont assistés de Replicants, androïdes que rien ne peut distinguer de l'être humain. Conçus comme de nouveaux esclaves, certains parmi les plus évolués s’affranchissent de leurs chaînes et s’enfuient. Rick Deckard, un ancien Blade Runner, catégorie spéciale de policiers chargés de \"retirer\" ces replicants, accepte une nouvelle mission consistant à retrouver quatre de ces individus qui viennent de regagner la Terre après avoir volé une navette spatiale. Ces replicants sont des Nexus 6, derniers nés de la technologie développée par la Tyrell Corporation.",
+            "category" => "Sciences-fiction",
+            "image" => "images/movies/Blade_Runner.jpg",
+        ],
+        [
+            "title" => "Forrest Gump",
+            "description" => "A Savannah, dans l'Etat de Géorgie, Forrest Gump, assis sur un banc public, livre à qui veut l'entendre, l'étrange récit de sa vie mouvementée. Il naît dans un bourg de l'Alabama, affecté d'un quotient intellectuel inférieur à la moyenne et d'une paralysie partielle des jambes. Souvent raillé à l'école, le jeune Forrest se lie d'amitié avec la belle Jenny. Ensemble, ils vont grandir dans l'Amérique des années 1960.",
+            "category" => "Drame",
+            "image" => "images/movies/Forrest_Gump.jpg",
+        ],
+        [
+            "title" => "Le Seigneur des Anneaux - Le Retour du roi",
+            "description" => "Guidés par Gollum, Frodon et Sam continuent leur périple vers la montagne du destin, tandis que Gandalf et ses compagnons se retrouvent à Isengard",
+            "category" => "Action",
+            "image" => "images/movies/Le_Seigneur_des_Anneaux_Le_Retour_du_roi.jpg",
+        ],
+        [
+            "title" => "The Dark Knight - Le Chevalier noir",
+            "description" => "Avec l'appui du lieutenant de police Jim Gordon et du procureur de Gotham, Harvey Dent, Batman vise à éradiquer le crime organisé qui pullule dans la ville. Leur association est très efficace, mais elle sera bientôt bouleversée par le chaos déclenché par un criminel psychopathe que les citoyens de Gotham connaissent sous le nom de Joker.",
+            "category" => "Action",
+            "image" => "images/movies/The_Dark_Knight_Le_Chevalier_noir.jpg",
+        ],
+        [
+            "title" => "Le Bon, la Brute et le Truand",
+            "description" => "Un chasseur de primes rejoint deux hommes dans une alliance précaire. Leur but ? Trouver un coffre rempli de pièces d'or dans un cimetière isolé",
+            "category" => "Western",
+            "image" => "images/movies/Le_Bon_la_Brute_et_le_Truand.jpg",
+        ],
+        [
+            "title" => "Inception",
+            "description" => "Dom Cobb est un voleur expérimenté, le meilleur dans l'art dangereux de l'extraction : spécialité qui consiste à voler les secrets les plus intimes enfouis au plus profond du subconscient durant une phase de rêve. Très recherché pour ses talents dans l’univers trouble de l’espionnage industriel, Cobb est aussi devenu un fugitif traqué dans le monde entier. Une ultime mission pourrait lui permettre de retrouver sa vie passée, accomplir une « inception ».",
+            "category" => "Action",
+            "image" => "images/movies/Inception.jpg",
+        ],
+        [
+            "title" => "Le Seigneur des Anneaux - La Communauté de l'anneau",
+            "description" => "Frodon reçoit l'Anneau de son oncle Bilbo. Sa vie et son monde sont pourtant en danger, car cet anneau appartient à Sauron, le maître des ténèbres",
+            "category" => "Aventure",
+            "image" => "images/movies/Le_Seigneur_des_Anneaux_La_Communaute_de_l_anneau.jpg",
+        ],
     ];
 
     /**
